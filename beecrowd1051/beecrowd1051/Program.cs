51c71314c80ff921d1978f6bf499a9159c2f8cf4
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-18 10:46:01 -0300

    C# Logica

---
+++ beecrowd1051/beecrowd1051/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd1051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double imposto;
+            if (salario <= 2000.0)
+            {
+                imposto = 0.0;
+            }
+            else if (salario <= 3000.0)
+            {
+                imposto = (salario - 2000.0) * 0.08;
+                // 0.08 representando uma forma simples de 8%
+            }
+            else if (salario <= 4500.0)
+            {
+                imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+                // 0.18 representando uma forma simples de 18%
+            }
+            else
+            {
+                imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+                // 0.28 representando uma forma simples de 28%
+            }
+
+            if (imposto == 0.0)
+            {
+                Console.WriteLine("Isento");
+            }
+            else
+            {
+                Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
