a16e53d82b600434511c963167355503ac8e1a3a
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-23 22:27:48 -0300

    C# Logica

---
+++ beecrowd1113/beecrowd1113/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string[] valores = Console.ReadLine().Split(' ');
+            x = int.Parse(valores[0]);
+            y = int.Parse(valores[1]);
+            
+            while (x != y) 
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                } else { Console.WriteLine("Decrescente");
+                }
+                valores = Console.ReadLine().Split(' ');
+                x = int.Parse(valores[0]);
+                y = int.Parse(valores[1]);
+            }
+        }
+    }
+}
