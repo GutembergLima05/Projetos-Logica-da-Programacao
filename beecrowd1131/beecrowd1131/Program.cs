c404f44a17c620e74fa162c1c98c3485551c5789
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-25 00:43:20 -0300

    C# Logica

---
+++ beecrowd1131/beecrowd1131/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int empate = 0, grenais = 0,vitoriaInter = 0, vitoriaGremio = 0;
+            int novoCalculo = 1;
+
+            while (novoCalculo == 1)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                int inter = int.Parse(valores[0]);
+                int gremio = int.Parse(valores[1]);
+
+                grenais++;
+
+                if (inter == gremio)
+                {
+                    empate++;
+                } else if (inter > gremio)
+                {
+                    vitoriaInter++;
+                } else
+                {
+                    vitoriaGremio++;
+                }
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                novoCalculo = int.Parse(Console.ReadLine());
+
+                while (novoCalculo != 1 && novoCalculo !=2)
+                {
+                    Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                    novoCalculo = int.Parse(Console.ReadLine());
+                }
+
+            }
+
+            Console.WriteLine(grenais + " grenais");
+            Console.WriteLine("Inter:" + vitoriaInter);
+            Console.WriteLine("Gremio:" + vitoriaGremio);
+            Console.WriteLine("Empates:" + empate);
+
+            if (vitoriaInter > vitoriaGremio)
+            {
+                Console.WriteLine("Inter venceu mais");
+            }
+            else
+            {
+                Console.WriteLine("Gremio venceu mais");
+            }
+
+        }
+    }
+}
