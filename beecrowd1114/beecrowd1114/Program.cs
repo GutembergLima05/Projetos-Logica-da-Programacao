e821cdfd140c3c9621cf4a33529cce4fc3f9795e
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-24 22:43:02 -0300

    C# Logica

---
+++ beecrowd1114/beecrowd1114/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            if (senha == 2002)
+            {
+                Console.WriteLine("Acesso Permitido");
+            }
+        }
+    }
+}
