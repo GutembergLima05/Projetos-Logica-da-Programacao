bbe9e020d3457591db7858d93625c7283843bceb
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-24 22:57:25 -0300

    C# Logica

---
+++ beecrowd1115/beecrowd1115/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            string[] valores = Console.ReadLine().Split(' ');
+
+            x = double.Parse(valores[0]);
+            y = double.Parse(valores[1]);
+
+            while (x != 0 && y != 0)
+            {
+                if (x > 0.0 && y < 0.0)
+                {
+                    Console.WriteLine("quarto");
+                }
+                else if (x > 0.0 && y > 0.0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if (x < 0.0 && y > 0.0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else
+                {
+                    Console.WriteLine("terceiro");
+               
+                }
+
+                valores = Console.ReadLine().Split(' ');
+                x = Double.Parse(valores[0]);
+                y = Double.Parse(valores[1]);
+            }
+        }
+    }
+}
