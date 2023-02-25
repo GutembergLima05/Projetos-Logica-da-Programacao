79658d12ef2f4744b167502c821bc3a505952c4b
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-24 23:41:25 -0300

    C# Logica

---
+++ beecrowd1159/beecrowd1159/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 2;
+
+            while (numero <= 100)
+            {
+                Console.WriteLine(numero);
+                numero = numero + 2;
+            }
+        }
+    }
+}
