20030b86c7ec77b5b6fc072fc81231861b361471
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-24 23:37:26 -0300

    C# Logica

---
+++ beecrowd1154/beecrowd1154/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cont = 0.0, soma = 0.0, dados;
+
+            dados = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (dados > 0)
+            {
+                cont++;
+                soma = soma + dados;
+                dados = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            dados = soma / cont;
+            Console.WriteLine(dados.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
