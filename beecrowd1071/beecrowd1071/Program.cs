ff2be4c31d5917def3f3cbb3bee307a2aef7e598
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-27 17:35:53 -0300

    Logica C#

---
+++ beecrowd1071/beecrowd1071/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min, max, soma = 0;
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                min = y;
+                max = x;
+            } else
+            {
+                min = x;
+                max = y;
+            }
+
+            for (int i=min+1; i<max; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine(soma);
+
+        }
+    }
+}
