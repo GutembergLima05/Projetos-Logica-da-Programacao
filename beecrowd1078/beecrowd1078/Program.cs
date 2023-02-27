4ac77055f761d0e42aeba0fed031dfbbdb8c96b3
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-27 17:35:08 -0300

    Logica C#

---
+++ beecrowd1078/beecrowd1078/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, calc;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                calc = i * N;
+                Console.WriteLine(i + " x " + N + " = " + calc);
+            }
+        }
+    }
+}
