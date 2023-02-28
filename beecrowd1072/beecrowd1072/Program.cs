e84db5aceb162bc5e9e39f2c65d89cf2a2b142a8
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-28 18:07:51 -0300

    C# Logica

---
+++ beecrowd1072/beecrowd1072/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int inn = 0;
+            int outt = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+              
+                if (x >= 10 && x <= 20)
+                {
+                    inn++;
+                } else
+                {
+                    outt++;
+                }
+            }
+
+            Console.WriteLine(inn + " in");
+            Console.WriteLine(outt + " out");
+        }
+    }
+}
