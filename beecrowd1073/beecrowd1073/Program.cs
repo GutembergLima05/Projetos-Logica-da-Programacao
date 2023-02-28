daba9d77cba564cdac0fc0048ad5d479eabc7acd
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-02-28 18:08:25 -0300

    C# Logica

---
+++ beecrowd1073/beecrowd1073/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                double calc = 0.0;
+                i++;
+                if (i % 2 == 0)
+                {
+                    calc = Math.Pow(i, 2.0);
+                }
+
+                Console.WriteLine(i.ToString("F0") + "^" + "2 = " + calc);
+            }
+        }
+    }
+}
