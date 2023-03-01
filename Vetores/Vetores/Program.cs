5db052708c6a253de99d9bf4ea1085d97bc005e9
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-03-01 18:03:22 -0300

    C# Logica

---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] vet;
+            vet = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[1]);
+                }
+            }
+        }
+    }
+}
