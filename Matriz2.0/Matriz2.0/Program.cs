91070816c54a51faea2a3140893dfd98f6942e66
GutembergLima05 <125606031+GutembergLima05@users.noreply.github.com>
2023-03-02 17:14:18 -0300

    C# Logica

---
+++ Matriz2.0/Matriz2.0/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matriz;
+            matriz = new int[n,n];
+            int cont = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+
+                for(int j =0; j < n; j++)
+                {
+                    matriz[i,j] = int.Parse(s[j]);
+
+                    if (matriz[i,j] < 0)
+                    {
+                        cont++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Diagonal Principal: ");
+
+            for (int i = 0; i <n; i++)
+            {
+                Console.Write(matriz[i,i] + " ");
+            }
+            Console.WriteLine();
+
+                Console.WriteLine("Numeros negativos é: " + cont);
+        }
+    }
+}
